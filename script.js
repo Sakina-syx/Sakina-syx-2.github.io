0acbec3886271d3a5638b292b73cbd93b163b0b2
Sakina-syx <486943635@qq.com>
2024-09-18 19:32:48 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,29 +1,39 @@
-let slideIndex = 1;
-showSlides(slideIndex);
+// 获取所有图片元素
+const images = document.querySelectorAll('.gallery img');
 
-function openModal() {
-    document.getElementById("myModal").style.display = "block";
-}
+// 创建一个模态框容器，用于显示大图
+const modal = document.createElement('div');
+modal.id = 'myModal';
+modal.innerHTML = `
+  <span id="closeModal" class="close">&times;</span>
+  <img class="modal-content" id="img01">
+`;
+document.body.appendChild(modal);
 
-function closeModal() {
-    document.getElementById("myModal").style.display = "none";
-}
+// 获取模态框中的图片元素
+const modalImg = document.getElementById('img01');
 
-function plusSlides(n) {
-    showSlides(slideIndex += n);
-}
+// 获取关闭模态框的元素
+const closeModal = document.getElementById('closeModal');
 
-function currentSlide(n) {
-    showSlides(slideIndex = n);
-}
+// 为每个图片添加点击事件
+images.forEach(image => {
+  image.onclick = function() {
+    // 设置模态框中图片的src属性为被点击图片的src
+    modalImg.src = this.src;
+    // 显示模态框
+    modal.style.display = "block";
+  };
+});
 
-function showSlides(n) {
-    let i;
-    let slides = document.getElementsByClassName("modal-content");
-    if (n > slides.length) {slideIndex = 1}
-    if (n < 1) {slideIndex = slides.length}
-    for (i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none";
-    }
-    slides[slideIndex-1].style.display = "block";
-}
+// 当用户点击关闭按钮时，隐藏模态框
+closeModal.onclick = function() {
+  modal.style.display = "none";
+};
+
+// 当用户点击模态框外部时，也隐藏模态框
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+};
